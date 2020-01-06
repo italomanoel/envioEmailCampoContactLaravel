d9de10cd49bd986d13d8fdd420393a94b723d1dd
italo manoel <manoel.italo89@gmail.com>
2020-01-06 18:25:12 -0300

    mudando web pontucacao errada

--- web.php
+++ web.php
@@ -4,4 +4,4 @@ Route::get('/envioemailS', function(){return view('envioEmailSucesso');});
 Route::get('/envio','EnvioEmailController@index');
 Route::post('/envio','EnvioEmailController@envioE');
 Route::resource('/', 'Home@index');
-});
+
